359e2214d0681589f5a21901c098c499f197047c
devanshukd04 <devanshukd27@gmail.com>
2023-02-26 15:03:52 +0530

    Added Boiler plate code

---
+++ src/main/java/com/example/ElectionProject/ElectionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ElectionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ElectionProject/ElectionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElectionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
