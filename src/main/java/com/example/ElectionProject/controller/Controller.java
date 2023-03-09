b6d3ab68a83b08b34b722ac50844fc4c8d6fd0fc
devanshukd04 <devanshukd27@gmail.com>
2023-03-09 13:48:16 +0530

    Added total number of pages and total number of results

--- src/main/java/com/example/ElectionProject/controller/Controller.java
+++ src/main/java/com/example/ElectionProject/controller/Controller.java
@@ -32,8 +32,9 @@ public class Controller {
 
     @GetMapping("/get")
     public ResponseEntity<?> getUser(@RequestParam(value="firstName") String firstName,@RequestParam(value="middleName") String middleName,@RequestParam(value="lastName") String lastName,@RequestParam(value="gender") String gender,@RequestParam(value="age") int age,@RequestParam(value="district") String district, @RequestParam(value="city") String city,@RequestParam(value="ward") String ward,@RequestParam(value="pageNo") int pageNo){
-        Pageable paging = PageRequest.of(pageNo, 40);
+        Pageable paging = PageRequest.of(pageNo, 2);
         List<Voter> voter=this.voterService.findBy(firstName,middleName,lastName,gender,age,district,city,ward,paging);
+        //voter.getTotalPages();
         return ResponseEntity.ok(voter);
     }
 
